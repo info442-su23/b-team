77ed176504ecfbf9e7aed03cc629b9380a462697
Robert Bonglamphone <bonglrob@uw.edu>
2023-07-19 18:55:32 -0700

    initial quiz completeion score page

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
+// https://www.youtube.com/watch?v=i7oL_K_FmM8
+// function setCookie(name, value){
+//   const date = new Date();
+//   date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000)
+//   let expires = "expires=" + date.toUTCString();
+//   document.cookie = `${name}=${value}; ${expires};`;
+// }
+
+// function getCookie(name){
+//   const cDecoded = decodeURIComponent(document.cookie);
+//   const cArray = cDecoded.split("; ");
+//   let result = null;
+
+//   cArray.forEach(element => {
+//     if(element.indexOf(name) == 0){
+//       result = element.substring(name.length + 1)
+//     }
+//   })
+// }
 
 export default App;
