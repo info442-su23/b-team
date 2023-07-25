4a6d92e841a621363873f644cde82efb12aec73a
wphan-1925132 <wphan@uw.edu>
2023-07-24 20:55:09 -0700

    react navbar update

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,16 +1,9 @@
+import NavBar from "./components/Nav.js"
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
     </div>
   );
 }
