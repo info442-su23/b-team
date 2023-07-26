730cf795cfa9d42c726cc8945ab9fd5a18f52e40
Robert Bonglamphone <bonglrob@uw.edu>
2023-07-26 08:37:06 -0700

    convert quiz instruction page to react component

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,9 +1,11 @@
 import NavBar from "./components/Nav.js"
+import QuizInstructions from "./components/QuizInstructions.js";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <QuizInstructions />
     </div>
   );
 }
