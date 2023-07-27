146232e92bed03fc072ef9c1ac5490b814e7e17d
Robert Bonglamphone <bonglrob@uw.edu>
2023-07-26 20:02:47 -0700

    comment out quizanswerbutton

--- react-project/src/components/QuizAnswer.js
+++ react-project/src/components/QuizAnswer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from './Nav';
 import BackButton from './GoBackButton';
-import QuizAnswerButton from './QuizAnswerButton';
 
 export default function QuizAnswer() {
   return (
@@ -10,7 +9,6 @@ export default function QuizAnswer() {
       <a href="#" className="back-arrow"><BackButton /></a>
       <h1>Question 1/5</h1>
       <h2>What percentage of trash is food?</h2>
-      <QuizAnswerButton />
       <section className="quiz-answers-container">
         <button className="answer-block">7</button>
         <button className="answer-block">22</button>
