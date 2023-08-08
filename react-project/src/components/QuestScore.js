eaa4dd3e61084e26045c8202f883d74008de2f9f
Robert Bonglamphone <bonglrob@uw.edu>
2023-08-07 18:15:14 -0700

    add points for quest

--- react-project/src/components/QuestScore.js
+++ react-project/src/components/QuestScore.js
@@ -1,8 +1,12 @@
 import NavBar from "./Nav"
 import BackButton from "./GoBackButton"
 import { Link } from "react-router-dom"
+import { useLocation } from 'react-router-dom';
 
 export default function QuestScore() {
+    const location = useLocation();
+    const totalPoints = location.state;
+
     return (
         <body>
             <NavBar />
@@ -10,11 +14,11 @@ export default function QuestScore() {
             <article id="quiz-score">
                 <section className="heading">
                     <h1>Sort</h1>
-                    <p>Congrats! You completed all the quest!.</p>
+                    <p>Congrats! You completed all the quest!</p>
                 </section>
                 <section className="energy-points">
                     <img className="energy" src="/img/sun.png" alt="sun" />
-                    <p>+ 10 Energy Points</p>
+                    <p>+ { totalPoints } Energy Points</p>
                 </section>
             </article>
             <section>
